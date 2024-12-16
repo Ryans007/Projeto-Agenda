070ae753ee27cb681894e458878e064f1ef8b1ba
Ryan <ryansoaresjorvino@gmail.com>
2024-12-16 13:46:12 -0300

    Criando View Contact

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from contact.models import Contact
-# Create your views here.
 
 def index(request):
   contacts = Contact.objects\
@@ -13,5 +12,16 @@ def index(request):
   return render(
     request,
     'contact/index.html',
+    context 
+  )
+
+def contact(request, contact_id):
+  single_contact = Contact.objects.get(pk=contact_id)
+  context = {
+     "contact": single_contact,
+  }
+  return render(
+    request,
+    'contact/contact.html',
     context 
   )
