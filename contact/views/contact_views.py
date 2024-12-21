4502badc487b4c0044b6741f8c285b711b931dab
Ryan <ryansoaresjorvino@gmail.com>
2024-12-21 16:11:39 -0300

    Iniciando a lógica para criação de contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
-from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
 
