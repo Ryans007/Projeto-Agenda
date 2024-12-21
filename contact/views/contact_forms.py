9ca514dc20d2a7fa6c8d9bb06c576b640fe24f53
Ryan <ryansoaresjorvino@gmail.com>
2024-12-21 16:47:46 -0300

    Utilizando o metodo Post no forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -5,6 +5,13 @@ from django.db.models import Q
 from django.core.paginator import Paginator
 
 def create(request):
+  if request.method == 'POST':
+      print()
+      print(request.method)
+      print(request.POST.get('first_name'))
+      print(request.POST.get('last_name'))
+      print()
+  
   context = {
     
   }
