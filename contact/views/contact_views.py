43789aae8e6a55861ef7678078e93604cc7c8bb3
Ryan <ryansoaresjorvino@gmail.com>
2024-12-21 15:07:48 -0300

    Melhorando o search com Q

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import get_object_or_404, render, get_list_or_404
+from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
 from django.http import Http404
+from django.db.models import Q
 
 def index(request):
   contacts = Contact.objects\
             .filter(show=True)\
-            .order_by("-id")[:10]
+            .order_by("-id")[10:20]
 
   context = {
      "contacts": contacts,
@@ -16,6 +17,33 @@ def index(request):
     'contact/index.html',
     context 
   )
+  
+def search(request):
+  search_value = request.GET.get('q', '').strip()
+  
+  if search_value == '':
+    return redirect('contact:index')
+
+  contacts = Contact.objects\
+            .filter(show=True)\
+            .filter(
+                    Q(first_name__icontains=search_value) |
+                    Q(last_name__icontains=search_value) |
+                    Q(phone__icontains=search_value) |
+                    Q(email__icontains=search_value) 
+                    )\
+            .order_by("-id")
+  
+  context = {
+     "contacts": contacts,
+     "site_title": "Search - "
+  }
+  return render(
+    request,
+    'contact/index.html',
+    context 
+  )
+
 
 def contact(request, contact_id):
   # single_contact = Contact.objects.filter(pk=contact_id).first()
