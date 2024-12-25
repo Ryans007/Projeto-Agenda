a542ac55f3a581f7d1f00d6484071c51147f356a
Ryan <ryansoaresjorvino@gmail.com>
2024-12-25 16:20:16 -0300

    Trabalhando com envio de messagens

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from contact.forms import RegisterForm
 
 def register(request):
@@ -6,8 +7,12 @@ def register(request):
   
   if request.method == 'POST':
     form = RegisterForm(request.POST)
+    
     if form.is_valid():
       form.save()
+      messages.success(request,'Usúario registrado')
+      return redirect('contact:index')
+    
   return render(
     request,
     'contact/register.html',
