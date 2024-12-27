60f0ee35772859fa8760011feb371ac6204877d7
Ryan <ryansoaresjorvino@gmail.com>
2024-12-27 20:13:00 -0300

    Relação entre contact e owner e garantindo que o usuário deve estar logado para acessar as views

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='contact:login')
 def create(request):
   form_action = reverse('contact:create')
   if request.method == 'POST':
@@ -14,6 +16,8 @@ def create(request):
       
       if form.is_valid():
         contact = form.save()
+        contact.owner = request.user
+        contact.save()
         return redirect('contact:update', contact_id=contact.pk)
       
       return render(
@@ -32,10 +36,10 @@ def create(request):
     context 
   )
 
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
   contact = get_object_or_404(
-    Contact, pk=contact_id, show= True
+    Contact, pk=contact_id, show= True, owner=request.user
   )
   form_action = reverse('contact:update', args=(contact_id,))
   
@@ -65,10 +69,11 @@ def update(request, contact_id):
     'contact/create.html',
     context 
   )
-  
+
+@login_required(login_url='contact:login')  
 def delete(request, contact_id):
   contact = get_object_or_404(
-    Contact, pk=contact_id, show= True
+    Contact, pk=contact_id, show= True, owner=request.user
   )
   confirmation = request.POST.get('confirmation','no')
   print('confirmation: ', confirmation)
